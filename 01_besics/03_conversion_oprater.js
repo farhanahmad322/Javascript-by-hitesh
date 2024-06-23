61faa5106c21978ffaa624b628904831c2950bf9
farhan ahmad <70254448+farhanahmad322@users.noreply.github.com>
2024-06-23 04:55:23 +0000

    conver

--- 01_besics/03_conversion_oprater.js
+++ 01_besics/03_conversion_oprater.js
@@ -1,11 +1,11 @@
 let score = "hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);  //string
+// console.log(typeof(score));  //string
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(typeof valueInNumber);  //number
+//console.log(valueInNumber);   //NaN
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+  //console.log(booleanIsLoggedIn);  //true
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,16 +24,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+ //console.log(typeof stringNumber);   //string
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+//console.log(negValue);
 
-// console.log(2+2);
+//console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
 // console.log(2**3);
@@ -60,9 +60,11 @@ let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
+//console.log(num1); // 4
+
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+//console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
