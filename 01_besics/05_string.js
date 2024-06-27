94854fef04ed9bbcd14a96460e7c603b685cad55
farhan ahmad <70254448+farhanahmad322@users.noreply.github.com>
2024-06-27 12:20:17 +0000

    string

---
+++ 01_besics/05_string.js
@@ -0,0 +1,33 @@
+const name = "Farhan "
+const repoCount = 50
+
+ //console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('farhan-Ah-com')
+
+//console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)      //farh
+//console.log(newString);      
+
+const anotherString = gameName.slice(-2, 3)
+console.log(anotherString);
+
+const newStringOne = "   Farhan    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+ //console.log(url.replace('%20', '-'))
+//console.log(url.includes('sundar'))   //fale
+//console.log(gameName.split('-'));   //[ 'farhan', 'Ah', 'com' ]
